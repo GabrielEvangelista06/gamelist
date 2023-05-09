3a00f1ddfc3a062d7595bc83ee2dc63b34b70385
GabrielEvangelista06 <gabrielevangelista094@gmail.com>
2023-05-09 17:18:39 -0300

    Setup base do projeto

---
+++ src/test/java/com/apollodev/gamelist/GamelistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apollodev.gamelist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamelistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
