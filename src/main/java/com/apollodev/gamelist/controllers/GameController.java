3dc1f85f043b976e7d02535f1179d877a1f85c3b
GabrielEvangelista06 <gabrielevangelista094@gmail.com>
2023-05-10 17:07:47 -0300

    Criando rota que retorna o Game baseado no id passado na rota

--- src/main/java/com/apollodev/gamelist/controllers/GameController.java
+++ src/main/java/com/apollodev/gamelist/controllers/GameController.java
@@ -1,10 +1,12 @@
 package com.apollodev.gamelist.controllers;
 
+import com.apollodev.gamelist.dto.GameDTO;
 import com.apollodev.gamelist.dto.GameMinDTO;
 import com.apollodev.gamelist.entities.Game;
 import com.apollodev.gamelist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +17,11 @@ import java.util.List;
 public class GameController {
     @Autowired
     private GameService gameService;
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id) {
+        return gameService.findById(id);
+    }
 
     @GetMapping
     public List<GameMinDTO> findAll() {
