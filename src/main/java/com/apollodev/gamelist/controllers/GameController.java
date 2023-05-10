76bbf4c40ff5ad7648ed351e57fd0ff809128945
GabrielEvangelista06 <gabrielevangelista094@gmail.com>
2023-05-09 23:41:55 -0300

    Criando GameController para se comunicar com o GameService

---
+++ src/main/java/com/apollodev/gamelist/controllers/GameController.java
@@ -0,0 +1,23 @@
+package com.apollodev.gamelist.controllers;
+
+import com.apollodev.gamelist.dto.GameMinDTO;
+import com.apollodev.gamelist.entities.Game;
+import com.apollodev.gamelist.services.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/games")
+public class GameController {
+    @Autowired
+    private GameService gameService;
+
+    @GetMapping
+    public List<GameMinDTO> findAll() {
+        return gameService.findAll();
+    }
+}
